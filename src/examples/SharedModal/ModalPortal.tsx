969b562a048891d4dcb855cc3e846cb93dbb6702
Maor Elimelech <maor700@gmail.com>
2021-10-14 13:10:38 +0300

    fix: modal portal remove elements after close

--- src/examples/SharedModal/ModalPortal.tsx
+++ src/examples/SharedModal/ModalPortal.tsx
@@ -1,6 +1,5 @@
-import React, { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { useCallback } from "react";
 /* eslint import/no-webpack-loader-syntax: off */
 // @ts-ignore
 import sharedModalCss from "!!raw-loader!./SharedModal.css";
@@ -9,22 +8,22 @@ export const ModalPortal: FC<{ css: string, name: string, isOpen?: boolean, posi
     const [modalElm, setModalElm] = useState<any>(null);
 
     useEffect(() => {
-        console.log(css);
-        init(css);
-    }, []);
-
-    const init = useCallback((css: string) => {
         const hostDoc = getHostDocument();
-        const modalElm = document.createElement("div");
-        modalElm.id = name;
-        modalElm.classList.add(name);
-        const shadowDom = modalElm.attachShadow({ mode: "closed" });
+        const modalElmTemp = document.createElement("div");
+        modalElmTemp.id = name;
+        modalElmTemp.classList.add(name);
+        const shadowDom = modalElmTemp.attachShadow({ mode: "open" });
         var style = document.createElement('style')
         style.innerHTML = css + sharedModalCss;
         shadowDom.appendChild(style);
         setModalElm(shadowDom);
-        hostDoc.body?.append(modalElm);
-    }, [])
+        hostDoc.body?.append(modalElmTemp);
+
+        return () => {
+            shadowDom.innerHTML = "";
+            modalElmTemp.remove()
+        }
+    }, []);
 
     return modalElm && ReactDOM.createPortal(
         children,
